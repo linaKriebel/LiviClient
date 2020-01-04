378d7f417280d640f0ec8eb9bd13236fbf4479e5
Lina <lina.kriebel@me.com>
2020-01-04 17:41:09 +0100

    receive level from server (WIP not working)

--- src/MessageListener.java
+++ src/MessageListener.java
@@ -1,13 +1,19 @@
 import models.GameEvent;
+import models.GameItem;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MessageListener implements Runnable{
+public class MessageListener implements Runnable {
 
     private Socket socket;
     private Client client;
     private ObjectInputStream inputStream;
+
+    private boolean isStartMessage = false;
+    private int count = 0;
 
     public MessageListener(Socket socket, Client client) throws IOException {
         this.client = client;
@@ -21,11 +27,14 @@ public class MessageListener implements Runnable{
             //Get the return message from the server
             try {
                 GameEvent event = (GameEvent) inputStream.readObject();
+                System.out.println("Message from server: " + event.getCommand());
                 switch (event.getCommand()) {
                     case START:
+                        //TODO fix damn OptionalDataException!!!
+                        //client.getWorld().setUp(event.getPlayers(), event.getBalls(), event.getObstacles(), event.getHoles());
                         client.getGui().startGame();
                     case MOVE:
-                        client.processMovement(event.getField(), event.getItemId(), event.getItemType());
+                        client.processMovement(event.getItemType(), event.getItemId(), event.getField());
                         break;
                     case REMOVE:
                         client.getWorld().removeBall(event.getItemId());
--- src/World.java
+++ src/World.java
@@ -1,5 +1,6 @@
 import models.Field;
 import models.GameEvent;
+import models.GameItem;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -16,48 +17,55 @@ public class World {
     public List<GameItem> holes = new ArrayList<>();
 
     public World(){
-        GameItem ai = new GameItem(new Field(1,1), 0, Color.CYAN);
-        GameItem player1 = new GameItem(new Field(2,2), 1, Color.MAGENTA);
-        GameItem player2 = new GameItem(new Field(6,6), 2, Color.YELLOW);
+//        GameItem ai = new GameItem(0, new Field(1,1), Color.CYAN);
+//        GameItem player1 = new GameItem(1, new Field(2,2), Color.MAGENTA);
+//        GameItem player2 = new GameItem(2, new Field(6,6), Color.YELLOW);
+//
+//        players.add(ai);
+//        players.add(player1);
+//        players.add(player2);
+//
+//        GameItem ball1 = new GameItem(1, new Field(7, 8), Color.WHITE);
+//        GameItem ball2 = new GameItem(2, new Field(3, 3), Color.WHITE);
+//
+//        balls.add(ball1);
+//        balls.add(ball2);
+//
+//        GameItem hole1 = new GameItem(1, new Field(10,10), Color.GREEN);
+//        GameItem hole2 = new GameItem(1, new Field(15,15), Color.GREEN);
+//
+//        holes.add(hole1);
+//        holes.add(hole2);
+//
+//        int idCount = 0;
+//        for(int x=0; x<WIDTH; x++){
+//            GameItem obstacle1 = new GameItem(idCount, new Field(x, 0), Color.darkGray);
+//            idCount++;
+//            GameItem obstacle2 = new GameItem(idCount, new Field(x, HEIGHT-1), Color.darkGray);
+//            idCount++;
+//
+//            obstacles.add(obstacle1);
+//            obstacles.add(obstacle2);
+//        }
+//        for(int y=0; y<HEIGHT; y++){
+//            GameItem obstacle1 = new GameItem(idCount, new Field(0, y), Color.darkGray);
+//            idCount++;
+//            GameItem obstacle2 = new GameItem(idCount, new Field(WIDTH-1, y), Color.darkGray);
+//            idCount++;
+//
+//            obstacles.add(obstacle1);
+//            obstacles.add(obstacle2);
+//        }
+//
+//        GameItem obstacle1 = new GameItem(idCount, new Field(5, 5), Color.darkGray);
+//        obstacles.add(obstacle1);
+    }
 
-        players.add(ai);
-        players.add(player1);
-        players.add(player2);
-
-        GameItem ball1 = new GameItem(new Field(7, 8), 1, Color.WHITE);
-        GameItem ball2 = new GameItem(new Field(3, 3), 2, Color.WHITE);
-
-        balls.add(ball1);
-        balls.add(ball2);
-
-        GameItem hole1 = new GameItem(new Field(10,10), 1, Color.GREEN);
-        GameItem hole2 = new GameItem(new Field(15,15), 1, Color.GREEN);
-
-        holes.add(hole1);
-        holes.add(hole2);
-
-        int idCount = 0;
-        for(int x=0; x<WIDTH; x++){
-            GameItem obstacle1 = new GameItem(new Field(x, 0), idCount, Color.darkGray);
-            idCount++;
-            GameItem obstacle2 = new GameItem(new Field(x, HEIGHT-1), idCount, Color.darkGray);
-            idCount++;
-
-            obstacles.add(obstacle1);
-            obstacles.add(obstacle2);
-        }
-        for(int y=0; y<HEIGHT; y++){
-            GameItem obstacle1 = new GameItem(new Field(0, y), idCount, Color.darkGray);
-            idCount++;
-            GameItem obstacle2 = new GameItem(new Field(WIDTH-1, y), idCount, Color.darkGray);
-            idCount++;
-
-            obstacles.add(obstacle1);
-            obstacles.add(obstacle2);
-        }
-
-        GameItem obstacle1 = new GameItem(new Field(5, 5), idCount, Color.darkGray);
-        obstacles.add(obstacle1);
+    public void setUp(List<GameItem> players, List<GameItem> balls, List<GameItem> obstacles, List<GameItem> holes){
+        this.players = players;
+        this.balls = balls;
+        this.obstacles = obstacles;
+        this.holes = holes;
     }
 
     public void setPlayerCoordinates(int id, Field position){
